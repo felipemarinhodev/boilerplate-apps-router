7da97a7b1e272d56cb21418a874cca5c4e110c70
Felipe Marinho <github@felipemarinho.dev>
2023-09-05 10:46:49 -0700

    install and configure jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/'],
+  transform: {
+    'ˆ.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  }
+};
