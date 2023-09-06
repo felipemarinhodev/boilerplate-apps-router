cf15525d793f38e8ae28bcb9c667e6c3d61044c2
Felipe Marinho <github@felipemarinho.dev>
2023-09-05 22:27:39 -0700

    add eslintignore"

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 const config = {
   staticDirs: ['../public'],
-  stories: ['../src/components/**/*.tsx'],
+  stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/nextjs',
